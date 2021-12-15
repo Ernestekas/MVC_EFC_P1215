b7ee7e79186bbec15ce5f3108eaf560a1e4be9d3
Ernestekas <ernestekas@gmail.com>
2021-12-15 22:09:12 +0200

    Exercise Configured everything for database connection
    Configured:
    Connection
    Models used for app creation

---
+++ ShopApp/ShopApp/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Models;
+
+namespace ShopApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Shop> Shops { get; set; }
+
+        public DbSet<ShopItem> ShopItems { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ShopApp/ShopApp/Models/Shop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ShopApp.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ShopItem> ShopItems { get; set; }
+    }
+}
